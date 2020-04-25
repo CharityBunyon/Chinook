37e634293093c43ef081f319974b79e07142e177
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-04-25 17:10:40 -0500

    completed exercise six

---
+++ Completed Exercises/sales_agent_invoices.sql
@@ -0,0 +1,37 @@
+-- Provide a query that shows the invoices associated with each sales agent. 
+--The result table should include the Sales Agent's full name.
+
+--SELECT * 
+--FROM Employee
+
+--SELECT * 
+--FROM Customer
+
+--SELECT *
+--FROM Invoice
+
+
+
+
+--SELECT 
+--  Employee.EmployeeId,
+--  Employee.LastName,
+--  Employee.FirstName,
+--  Customer.SupportRepId
+--FROM Employee
+--	JOIN Customer
+--		ON Customer.SupportRepId = Employee.EmployeeId
+--	JOIN Invoice
+--		ON Invoice.CustomerId = Customer.CustomerId
+--WHERE Title = 'Sales Support Agent'
+
+
+SELECT
+	CONCAT(e.FirstName, ' ', e.LastName) as EmployeeName, 
+	e.Title,
+	e.EmployeeId,
+	c.SupportRepId,
+	i.InvoiceId
+FROM Invoice i
+	JOIN Customer c on c.CustomerId=i.CustomerId
+	JOIN Employee e on e.EmployeeId=c.SupportRepId
