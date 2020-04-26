59e7814b3b56badf87c05dee7df1a4a53ebb8f86
Charity Bunyon <CSEYMOU5@GMAIL.com>
2020-04-25 23:28:53 -0500

    completed exercises 11-15

---
+++ Completed Exercises/playlists_track_count.sql
@@ -0,0 +1,19 @@
+-- Provide a query that shows the total number of tracks in each playlist.
+-- The Playlist name should be include on the resultant table.
+-- Need to JOIN PlaylistTrack & Playlist
+
+SELECT *
+FROM PlaylistTrack
+
+SELECT *
+FROM Playlist
+
+
+
+SELECT 
+	COUNT (pt.TrackId) AS NumberOfTracks,
+	p.Name
+FROM PlaylistTrack pt
+	JOIN Playlist p
+		ON pt.PlaylistId = p.PlaylistId
+GROUP BY p.Name
